0f4ae0505b1421f93359422a92dff61f07dde6c9
Abhishek-Royy <abhishek699.roy@gmail.com>
2026-09-13 01:34:18 +0530

    feat: Import chat routes and set up router in chat service

--- backend/services/chat/index.js
+++ backend/services/chat/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import router from "./routes/chat.routes.js";
 
 
 dotenv.config();
@@ -8,9 +9,13 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Hello from Chat");
-});
+
+// use the routers
+app.use("/",router)
+
+// app.get("/", (req, res) => {
+//   res.send("Hello from Chat");
+// });
 
 
 
