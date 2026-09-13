6684d193bcbc9cbfa6939992f6486d0f8984e0e5
Abhishek-Royy <abhishek699.roy@gmail.com>
2026-09-13 23:15:08 +0530

    feat: Add agent service files and initialize agent server

---
+++ backend/services/agent/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import router from "./routes/agent.route.js";
+
+dotenv.config();
+const PORT = process.env.PORT;
+const app = express();
+app.use(express.json());
+
+// agent er route ata
+app.use("/", router);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Agent");
+});
+
+app.listen(PORT, () => {
+  console.log(`Agent started at http://localhost:${PORT}`);
+  connectDB();
+});
