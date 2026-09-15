cd38c801de2367e53a7eae4d04e0815a2b1d0993
Abhishek-Royy <abhishek699.roy@gmail.com>
2026-09-15 21:45:29 +0530

    littilebit something create

--- frontend/src/components/Artifact.jsx
+++ frontend/src/components/Artifact.jsx
@@ -19,7 +19,7 @@ function Artifact() {
   }
 
   return (
-    <div className="w-80 h-full bg-slate-950 border-l border-slate-800 flex flex-col">
+    <div className="w-100 h-full bg-slate-950 border-l border-slate-800 flex flex-col">
       <div className="border-b border-slate-800 px-3 py-2.5 flex items-center justify-between">
         <button onClick={() => setCollapsed(true)}>
           <FiChevronRight
--- frontend/src/redux/consversationSlice.js
+++ frontend/src/redux/consversationSlice.js
@@ -1,9 +1,34 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const conversationSlice = createSlice({
+//   name: "conversation",
+//   initialState: {
+//     conversations: [],
+//     selectedConversation:null
+//   },
+//   reducers: {
+//     setConversations: (state, action) => {
+//       state.conversations = action.payload;
+//     },
+//     addConversation: (state, action) => {
+//       state.conversations.unshift(action.payload);
+//     },
+//     setSelectedConversation: (state, action) => {
+//       state.selectedConversation.unshift(action.payload);
+//     },
+//   },
+// });
+
+// export const {setConversations,addConversation,setSelectedConversation} = conversationSlice.actions;
+// export default conversationSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const conversationSlice = createSlice({
   name: "conversation",
   initialState: {
     conversations: [],
+    selectedConversation: null,
   },
   reducers: {
     setConversations: (state, action) => {
@@ -12,8 +37,12 @@ const conversationSlice = createSlice({
     addConversation: (state, action) => {
       state.conversations.unshift(action.payload);
     },
+    setSelectedConversation: (state, action) => {
+      state.selectedConversation = action.payload;
+    },
   },
 });
 
-export const {setConversations,addConversation} = conversationSlice.actions;
+export const { setConversations, addConversation, setSelectedConversation } =
+  conversationSlice.actions;
 export default conversationSlice.reducer;
